b13e7978c46ef5911d5bb7a421bd8a58bd4f80da
havor13 <sampsonhavor43@gmail.com>
2026-02-12 04:32:52 +0000

    Update .gitignore to ignore node_modules, build, and .env

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // optional global styles
+import './styles/index.css';
+import App from './App';
 
 // Create root and render App
 const root = ReactDOM.createRoot(document.getElementById("root"));
